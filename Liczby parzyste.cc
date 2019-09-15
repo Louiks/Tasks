1dd4e30f49ebec2b7146c45cf84fc45a69b486ae
Louiks <50913008+Louiks@users.noreply.github.com>
2019-09-16 00:14:12 +0200

    Add files via upload

--- Liczby parzyste.cc
+++ Liczby parzyste.cc
@@ -1,27 +1,28 @@
 #include <iostream>
 
-int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int array[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
 int x;
 
 void myFunct()
 {
     int y = array[x] % 2;
-    x++;
+    
     if (y ==0)
     {
-        std::cout << array[x] << " jest nieparzyste" << std::endl;
+        std::cout << array[x] << " jest parzyste" << std::endl;
         
     }
     else 
     {
-        std::cout << array[x] << " jest parzyste"<< std::endl;
+        std::cout << array[x] << " jest nieparzyste"<< std::endl;
     }
+    x++;
 }
 
 
 int main()
     {
-    for( x; x<10; myFunct());
+    for( x=0; x<9; myFunct());
 
 
         return 0;
