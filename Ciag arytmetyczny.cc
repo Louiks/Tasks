76f03ae8741318a40d15f8f006f3fc757334c81f
Paul <45698887+PaulTony7@users.noreply.github.com>
2019-09-19 11:46:16 +0200

    Update Ciag arytmetyczny.cc

--- Ciag arytmetyczny.cc
+++ Ciag arytmetyczny.cc
@@ -1,11 +1,16 @@
 #include <iostream>
+// nie deklaruj zmiennych globalnie!!!
 int x;
 int main()
 {
     std::cout << "Który wyraz ciągu?" << std::endl;
     std::cin >>  x;
-    int y = 9+(x-1)*7;
+    int y = 9+(x-1)*7;//format
     std::cout << x << " Wyraz ciągu to " << y;
 
     return 0;
 }
+//Ogólnie to te zadanie trochę za krótkie, żeby je mocno zmieniać,
+//mógłbyś to teoretycznie wyciągnąć z main'a do jakiejś innej funkcji
+//i ją z tamtego miejsca wykonać, no ale to takie czepianie się,
+//dlatego że tak jest poprawnie w dużych projektach :)
