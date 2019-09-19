882b7e61135f93178760c18e9d6f7d3e3abb2323
Louiks <50913008+Louiks@users.noreply.github.com>
2019-09-19 23:05:27 +0200

    Add files via upload

--- NWD.cc
+++ NWD.cc
@@ -1,7 +1,24 @@
 #include <iostream>
 
-int main()
+int a =0;
+int b;
+int x;
+int y;
+
+for (;a<x;a++)
 {
     
+}
+
+int main()
+{
+    std::cout << "Podaj dwie zmienne " << std::endl;
+
+    std::cin >>  x >>  y;
+    
+    
+
+    std::cout << "nwd to " << b;
+
     return 0;
 }
