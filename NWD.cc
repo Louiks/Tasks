a4f2355012fbbb2cd47d6cc24e918c6b4c15e07d
Louiks <50913008+Louiks@users.noreply.github.com>
2019-09-20 22:55:20 +0200

    Add files via upload

--- NWD.cc
+++ NWD.cc
@@ -1,24 +1,51 @@
 #include <iostream>
 
-int a =0;
-int b;
-int x;
-int y;
-
-for (;a<x;a++)
+void xD(int x, int y)
 {
-    
+    int c;
+    if (x > y)
+    {}
+    else 
+    {
+        c = x;
+        x = y;
+        y = c;
+    }
+
+}    
+
+void myFunc(int x, int y, int b)
+{
+   
+    if (x % y)
+    {
+        b = x % y;
+        y = b;
+    }    
+    else
+    {   b = x;
+        x = y;
+        y = b % y;
+    }
 }
 
 int main()
 {
+    int x;
+    int y;
+    int b;
+    xD ( x, y);
     std::cout << "Podaj dwie zmienne " << std::endl;
 
-    std::cin >>  x >>  y;
+    std::cin >> x >> y;
     
-    
+    while () 
+    {
+     myFunc(x, y, b);   
+    }
 
-    std::cout << "nwd to " << b;
+    std::cout << "nwd to " << b <<std::endl;
 
-    return 0;
+    return 0;//btw zmieniłem adres zamieszkania
+
 }
