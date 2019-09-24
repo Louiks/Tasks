94ccaf26eb0246308846ef77fd93330c8005ba91
Louiks <50913008+Louiks@users.noreply.github.com>
2019-09-24 03:07:38 +0200

    Add files via upload

--- NWD.cc
+++ NWD.cc
@@ -1,15 +1,5 @@
 #include <iostream>
 
-void myFunc (int x, int y, int a)
-{
-    while (a != 0)
-    {
-        x = y;
-        y = a;
-        a = x % y;
-
-    }
-}
 
 int main()
 {
@@ -17,7 +7,13 @@ int main()
     std::cin >> x >> y;
 
     a = x % y;
-    myFunc(x, y, a);
+    while (a != 0)
+    {
+        x = y;
+        y = a;
+        a = x % y;
+
+    }
     std::cout << "NWD to " << y << std::endl;
 
     return 0;
