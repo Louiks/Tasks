b100743bbd8caa181e4f5d1986b68b1bca1a7601
Louiks <50913008+Louiks@users.noreply.github.com>
2019-10-03 22:57:06 +0200

    Add files via upload
    nie działa

--- idk.cc
+++ idk.cc
@@ -1,44 +1,40 @@
 #include <iostream>
 
+void myFunc(int x, int y, int p)
+{
+    int z;
+    while (x != y)
+    {
+        z = x;
+        if(z % p != 0)
+        {
+            while(z > p)
+            {
+                z = z - p;
+                if (z%p == 1)
+                {
+                    std::cout << x <<" oraz " << p << "to liczby względnie pierwsze" << std::endl;
+                }
+            }
+        }
+        if(x < y)
+            {x++;}
+        else
+            {x--;}
+        //std::cout << "a" << std::endl;
+    }
+
+
+}
+
 int main()
 {
-    int x = 99;
-    int y;
-    std::cout << "Take points between 1 and 5 and try to beat your opponent by forcing him to pick last points, game starts at 99 points." << std::endl;
-    while (x != 0)
-    {
-        
-        std::cin >> y;
-        x -= y;
-        switch ()
-        {
-        case 1: 
-        y == 5;
-            break;
-        case 2:
-        y == 4;
-        x -= 1;
-            break;
-        case 3:
-        y == 3;
-        x -= 2;
-            break;
-        case 4:
-        y == 2;
-        x -= 1;
-            break;
-        case 5:
-        y == 1;
-        x -= 0;    
-            break;
-        case 6:
-        y == 0;
-            break;        
-
-        default:
-            break;
-        }
-    }
     
-    return o;
+    int x, y, p;
+    std::cout << "nadaj wartosc x, y oraz p" << std::endl;
+    std::cin >> x >> y >> p;
+    myFunc(x, y, p);
+
+    
+    return 0;
 }
