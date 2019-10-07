06b9dfedd49efab0f0ce9eaa42b787eea9e3f5d0
Louiks <50913008+Louiks@users.noreply.github.com>
2019-10-07 23:57:34 +0200

    Add files via upload
    written in 3 minutes

---
+++ obwkola.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int r,o;
+    std::cout<<"podaj promien koła"<<std::endl;
+    std::cin>> r;
+    o=3*r*r;
+    std::cout<< "obw kola wynosi "<< o<<std::endl;
+    return 0;
+}
